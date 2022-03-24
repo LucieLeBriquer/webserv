3555b2d2f91359ea0fff0a4ca676bf8a5b82a833
Lou PASCREAU <loupascreau@gmail.com>
2022-03-24 18:02:52 +0100

    server creer

--- server.cpp
+++ server.cpp
@@ -13,9 +13,10 @@ int		main(void)
 	socklen_t	   	    len_addr;
     char                buf[1024] = {0};
     int                 byte_count;
+    int                 yes = 1;
 
     len_addr = sizeof(address);
-	if (server_fd = socket(AF_INET, SOCK_STREAM, 0) < 0)
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
 		std::cout << "Error with socket()" << std::endl;
 		exit(EXIT_FAILURE);
@@ -26,6 +27,11 @@ int		main(void)
     address.sin_port = htons(PORT);
     memset(address.sin_zero, '\0', sizeof(address.sin_zero));
 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &yes, sizeof(yes)) < 0)
+    {
+        std::cout << "Error with setsockopt()" << std::endl;
+        exit(EXIT_FAILURE);
+    }
     if (bind(server_fd, (struct sockaddr *)&address, (int)len_addr) < 0)
     {
         std::cout << "Error with bind()" << std::endl;
@@ -51,7 +57,7 @@ int		main(void)
         std::cout << buf << std::endl;
         send(new_socket, "Hello world!", 12, 0);
         std::cout << "---------- Hello message sent ----------" << std::endl;
-        close(new_socket);
+        // close(new_socket);
     }
     
 	return 0;
