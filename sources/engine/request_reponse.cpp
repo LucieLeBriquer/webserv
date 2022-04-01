48825fda84fb2cf035eac03ed4d2ba4f1222c2d9
lou pascreau <loupascreau@gmail.com>
2022-04-01 10:31:23 +0200

    implementation de recv pour read plusieurs ligne du header de requete avant de recv totalement

--- sources/engine/request_reponse.cpp
+++ sources/engine/request_reponse.cpp
@@ -6,24 +6,37 @@
 /*   By: lpascrea <lpascrea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 10:15:59 by lpascrea          #+#    #+#             */
-/*   Updated: 2022/03/31 18:12:44 by lpascrea         ###   ########.fr       */
+/*   Updated: 2022/04/01 10:29:48 by lpascrea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "engine.hpp"
 
+int		sendReponse(int fde)
+{
+	std::string hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!\n";
+
+	if (send(fde, hello.c_str(), hello.size(), 0) < 0)
+	{
+		perror("send()");
+		return -1;
+	}
+	std::cout << "sending data to " << fde << std::endl;
+	/*si code erreur (bad request ou autre) -> close(fde), si code succes on ne close pas le fd*/
+	close(fde);
+	return 1;
+}
+
 int		requestReponse(int epollfd, int fde)
 {
-	char		buf[2] = {0};
-	int			byteCount;
-	// std::string string;
+	char		buf[BUFFER_SIZE] = {0};
+	int			byteCount, recv_len = 0;
+	std::string	string;
 
-	memset(buf, 0, sizeof(buf));
 	while (1)
 	{
-		byteCount = recv(fde, buf, sizeof(buf), MSG_DONTWAIT);
-		// string += buf;
-		// std::cout << "byteCounte = " << byteCount << "  string = " << string << std::endl;
+		memset(buf, 0, BUFFER_SIZE);
+		byteCount = recv(fde, buf, BUFFER_SIZE, 0);
 		if (byteCount == 0)
 		{
 			epoll_ctl(epollfd, EPOLL_CTL_DEL, fde, NULL);
@@ -31,22 +44,22 @@ int		requestReponse(int epollfd, int fde)
 		}
 		else if (byteCount < 0)
 		{
-			close(fde);
-			break ;
+			/*verifier chaque ligne si il s'agit d'une requete valide, si non, on break et bad request
+			if (badRequest(string))
+				break ; 
+			*/
+			if (strcmp(&string[string.length() - 4], "\r\n\r\n") == 0)
+				break ;
 		}
 		else
 		{
-			std::string hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!\n";
-
-			std::cout << "get " << byteCount << " bytes of content from " << fde << " [ " << buf << " ] " << std::endl;
-			if (send(fde, hello.c_str(), hello.size(), MSG_DONTWAIT) < 0)
-			{
-				perror("send()");
-				return -1;
-			}
-			std::cout << "sending data to " << fde << std::endl;
-			close(fde);
+			recv_len += byteCount;
+			std::cout << "get " << recv_len << " bytes from " << fde << " : buf = [ " << buf << " ] " << std::endl;
 		}
+		string += buf;
 	}
+	std::cout << "final string = " << string << std::endl;
+	if (sendReponse(fde) < 0)
+		return -1;
 	return 1;
 }
