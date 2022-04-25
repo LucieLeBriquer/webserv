327fa320a354263b2f84ec7b7c0597ab7d3d48a2
lou pascreau <loupascreau@gmail.com>
2022-04-25 18:05:01 +0200

    creation dun fichier pour lenv du cgi, cgi mit en place en cas de post request ou get

--- sources/engine/cgi.cpp
+++ sources/engine/cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 14:43:44 by lpascrea          #+#    #+#             */
-/*   Updated: 2022/04/22 15:35:58 by user42           ###   ########.fr       */
+/*   Updated: 2022/04/25 18:00:51 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,81 +19,11 @@
  * STDIN == 0 & STDOUT == 1
  ********/
 
-void	ft_free_env_arg(char ***env, char ***arg, Socket *sock)
-{
-	size_t i = 0;
-
-	while (i < sock->getEnvSize())
-	{
-		free((*env)[i]);
-		(*env)[i] = NULL;
-		i++;
-	}
-	free((*env));
-	(*env) = NULL;
-	i = 0;
-	while (i < 3)
-	{
-		free((*arg)[i]);
-		(*arg)[i] = NULL;
-		i++;
-	}
-	free((*arg));
-	(*arg) = NULL;
-}
-
-int 	mallocEnv(char ***env, Socket &sock)
-{
-	size_t i = 0, j = 0, nbr = 0;
-	std::string	tmp = sock.getEnv();
-
-	(*env) = (char **)malloc(sizeof(char *) * sock.getEnvSize() + 1);
-	if (!(*env))
-		return ERR;
-	while (tmp[i])
-	{
-		j = i;
-		while (tmp[i] && tmp[i] != '!')
-			i++;
-		(*env)[nbr] = (char *)malloc(sizeof(char) * ((i - j) + 1));
-		if (!(*env)[nbr])
-			return ERR;
-		strncpy((*env)[nbr], &tmp[j], i - j);
-		(*env)[nbr][i - j] = '\0';
-		nbr++;
-		i++;
-	}
-	return OK;
-}
-
-int 	mallocArg(char ***arg, std::string cgi, std::string file)
-{
-	(*arg) = (char **)malloc(sizeof(char *) * 3);
-	if (!(*arg))
-		return ERR;
-		
-	/** Malloc executable **/
-	(*arg)[0] = (char *)malloc(sizeof(char) * (strlen(cgi.c_str()) + 1));
-	if (!(*arg)[0])
-		return ERR;
-	strcpy((*arg)[0], cgi.c_str());
-	
-	/** Malloc file asked **/
-	(*arg)[1] = (char *)malloc(sizeof(char) * (strlen(file.c_str()) + 1));
-	if (!(*arg)[1])
-		return ERR;
-	strcpy((*arg)[1], file.c_str());
-	
-	/** last arg NULL **/
-	(*arg)[2] = NULL;
-	return OK;
-}
-
-int 	GetCGIfile(Socket &sock, int sockNbr, std::string cgi, std::string file)
+int 	GetCGIfile(Socket &sock, int fde, std::string cgi, std::string file)
 {
 	char **env, **arg;
 	pid_t pid;
-	int status;
+	int status, fd[2];
 	std::string body;
 
 	body = sock.getBody();
@@ -103,31 +33,62 @@ int 	GetCGIfile(Socket &sock, int sockNbr, std::string cgi, std::string file)
 	if (mallocArg(&arg, cgi, file) < 0)
 		return ERR;
 
+	/////////////////printing////////////////
+	std::cout << "=== ENV ===" << std::endl;
+	mapStr	tmp = sock.getEnv();
+	mapStr::iterator it = tmp.begin();
+	for (size_t i = 0; env[i]; i++)
+	{
+		if (it == tmp.end())
+			break ;
+		std::cout << "i = " << i << "  " << env[i] << std::endl;
+		// std::cout << "s = " << sock.getEnvSize() << "  " << it->first << "=" << it->second << std::endl;
+		it++;
+	}
+	std::cout << "===========" << std::endl;
+	/////////////////////////////////////////
+	
+	if (sock.getEnvValue("REQUEST_METHOD") == "POST")
+		pipe(fd);
 	pid = fork();
 	if (pid < 0)
 		exit(EXIT_FAILURE);
 	if (pid == 0)
 	{
-		dup2(sockNbr, STDOUT_FILENO); // our connected socket become stdout
+		if (sock.getEnvValue("REQUEST_METHOD") == "POST")
+		{
+			close(fd[1]);
+			dup2(fd[0], STDIN_FILENO);
+		}
+		dup2(fde, STDOUT_FILENO); // our connected socket become stdout
 		execve(arg[0], arg, env);
 	}
 	else
 	{
-		close(sockNbr);
-		waitpid(pid, &status, 0);
+		if (sock.getEnvValue("REQUEST_METHOD") == "POST")
+		{
+			close(fd[0]);
+			write(fd[1], sock.getBody().c_str(), strlen(sock.getBody().c_str()));
+		}
+		close(fde);
 	}
+	close(fd[1]);
+	waitpid(pid, &status, 0);
 	
-	// printing
+	/////////////////printing////////////////
 	std::cout << std::endl;
 	std::cout << "================ inside cgi function ===============" << std::endl;
 	std::cout << std::endl;
 	std::cout << " - arg[0] = " << arg[0] << std::endl;
 	std::cout << " - arg[1] = " << arg[1] << std::endl;
-	std::cout << " - connected socket = " << sockNbr << std::endl;
+	std::cout << " - connected socket = " << fde << std::endl;
 	std::cout << " - body = " << sock.getBody() << std::endl;
 	std::cout << "====================================================" << std::endl;
 	std::cout << std::endl;
+	/////////////////////////////////////////
 	
 	ft_free_env_arg(&env, &arg, &sock);
+	sock.unsetEnv();
+	sock.setIsQueryString(false);
 	return OK;
 }
