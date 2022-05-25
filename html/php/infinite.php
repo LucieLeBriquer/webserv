27f614d4c403b73f27aee5f7d4290c27de8779a4
Lou PASCREAU <loupascreau@gmail.com>
2022-05-25 14:40:18 +0200

    infinite true

--- html/php/infinite.php
+++ html/php/infinite.php
@@ -1,12 +1,10 @@
 <?php
 
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	// ini_set('error_reporting', E_ALL);
+	// ini_set('display_errors', 1);
+	// ini_set('display_startup_errors', 1);
 	while (true)
 	{
-		if (error_reporting(E_ALL))
-			print("echo reporting error");
+		// error_reporting(E_ALL);
 	}
 
 ?>
